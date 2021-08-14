94ca899b98ed6f3c8b3a9e6cb84031f0cd2aa1b5
Otieno Calvine <nyarangaotieno@gmail.com>
2021-08-15 02:29:13 +0300

    feat: initialize todo app
    Signed-off-by: Otieno Calvine <nyarangaotieno@gmail.com>

---
+++ todos/views.py
@@ -0,0 +1,20 @@
+import todos
+from django.http import HttpRequest
+from django.shortcuts import render, redirect
+from .models import Todo
+
+def list_todo_items(request):
+    todos = Todo.objects.all()
+    context = {'todo_list': todos}
+
+    return render(request, 'todos/todo_list.html', context)
+
+def insert_todo_item(request:HttpRequest):
+    todo = Todo(content = request.POST['content'])
+    todo.save()
+    return redirect('/todos/list/')
+
+def delete_todo_item(request, todo_id):
+    todo_to_delete = Todo.objects.get(id=todo_id)
+    todo_to_delete.delete()
+    return redirect('/todos/list/')
